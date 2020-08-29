2fd099b66c09ef9275e23c0db6bbe6d996146caa
priya4110 <priyatoshpatra@gmail.com>
2020-08-29 22:46:42 +0530

    Updates

---
+++ precache-manifest.26616f5c12c5510621147b3c9cacf71d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9291d06fe2f21acb502b24b5647ce4d",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "5d045e718f917fda7ef6",
+    "url": "/react-tic-tac-toe/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "2459a9a976df5a4b2cea",
+    "url": "/react-tic-tac-toe/static/css/main.d993e80e.chunk.css"
+  },
+  {
+    "revision": "5d045e718f917fda7ef6",
+    "url": "/react-tic-tac-toe/static/js/2.28908e7f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-tic-tac-toe/static/js/2.28908e7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2459a9a976df5a4b2cea",
+    "url": "/react-tic-tac-toe/static/js/main.60ee9ca1.chunk.js"
+  },
+  {
+    "revision": "929d476f7b1ee9149c3a",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.db7cfeb9.js"
+  },
+  {
+    "revision": "acc667d51ac4ba55d7fa71db9f31cd34",
+    "url": "/react-tic-tac-toe/static/media/TicTacToe.acc667d5.svg"
+  }
+]);
